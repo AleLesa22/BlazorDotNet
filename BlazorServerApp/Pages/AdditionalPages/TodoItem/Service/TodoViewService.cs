713fdf4878da5f8a4dbb89aea6c681a707afd715
Aleksandar Vidakovic <aleksandar.vidakovic7422@gmail.com>
2022-11-18 09:29:39 +0100

    making add todo work

--- BlazorServerApp/Pages/AdditionalPages/TodoItem/Service/TodoViewService.cs
+++ BlazorServerApp/Pages/AdditionalPages/TodoItem/Service/TodoViewService.cs
@@ -4,38 +4,28 @@ namespace BlazorServerApp.Pages.AdditionalPages.TodoItem.Service
 {
     public class TodoViewService
     {
-
         public static List<CategoryDTO> categories = new List<CategoryDTO>();
         public static List<UserDTO> users = new List<UserDTO>();
-        public async Task InsertTodo(string Title, string Description, bool IsDone, string CategoryId, string CategoryName, string UserId, string UserFirstName, string UserLastName, HttpClient httpClient)
+
+        public async Task InsertTodo(string Title, string Description, bool IsDone, string CategoryId, string UserId, HttpClient httpClient)
         {
+            categories = await httpClient.GetFromJsonAsync<List<CategoryDTO>>("https://localhost:7127/api/Categories");
+            users = await httpClient.GetFromJsonAsync<List<UserDTO>>("https://localhost:7127/api/Users");
+
             TodoItemDTO newTodoItem = new TodoItemDTO();
-            CategoryDTO Cat = new CategoryDTO();
-            UserDTO Usr = new UserDTO();
-
-            HttpClient httpClientGetUsers = new HttpClient();
-            HttpClient httpClientGetCategories = new HttpClient();
-            HttpClient newHttpCLIENT = new HttpClient();
-
-            categories = await httpClientGetCategories.GetFromJsonAsync<List<CategoryDTO>>("https://localhost:7127/api/Categories");
-            users = await httpClientGetUsers.GetFromJsonAsync<List<UserDTO>>("https://localhost:7127/api/Users");
+            CategoryDTO cat = new CategoryDTO();
+            cat = categories.First(x => x.Id == CategoryId);
+            UserDTO usr = new UserDTO();
+            usr = users.First(x => x.Id == UserId);
 
             newTodoItem.Id = Guid.NewGuid().ToString();
             newTodoItem.Title = Title;
             newTodoItem.Description = Description;
             newTodoItem.IsDone = IsDone;
+            newTodoItem.Category = cat;
+            newTodoItem.User = usr; ;
 
-            Cat.Id = CategoryId;
-            Cat.Name = CategoryName;
-
-            Usr.Id = UserId;
-            Usr.FirstName = UserFirstName;
-            Usr.LastName = UserLastName;
-
-            newTodoItem.Category = Cat;
-            newTodoItem.User = Usr;
-
-            var result = await newHttpCLIENT.PostAsJsonAsync("https://localhost:7127/api/TodoItem", newTodoItem);
+            var result = await httpClient.PostAsJsonAsync("https://localhost:7127/api/TodoItem", newTodoItem);
         }
     }
 }
